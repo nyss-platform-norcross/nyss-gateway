d9d5310a4e75baba94b578c5ce7098a796972336
Jascha Riedel <jaschelite@googlemail.com>
2019-10-19 12:18:51 +0200

    Finalized GSM Adapter

--- backend/gsmadapter.py
+++ backend/gsmadapter.py
@@ -1,12 +1,39 @@
+import huaweiaccess
+import sim868WaveshareAccess
+import os
+
+
+
+MOCK_STICK = -1
+HUAWEI_LTE_STICK = 0
+SIM868_WAVESHARE_GSM = 1
+
+if os.get('GSM_STICK') is None:
+    GSM_MODULE = MOCK_STICK
+else:
+    try:
+        GSM_MODULE = int(os.get('GSM_STICK'))
+    except Exception:
+        print("Enviornment Variable GSM_STICK must be -1,0,1!!!")
+
 
 
 
 def isPinRequired():
-    return True
+    if GSM_MODULE is HUAWEI_LTE_STICK:
+        return huaweiaccess.isPinRequired()
+    elif GSM_MODULE is SIM868_WAVESHARE_GSM:
+        raise RuntimeError("Not Implemented")
+    elif GSM_MODULE is MOCK_STICK:
+        return True
 
 def unlockWithPin(pin):
-    if pin == 5252:
-        return True
-    else:
-        return False
-
+    if GSM_MODULE is HUAWEI_LTE_STICK:
+        return huaweiaccess.unlockWithPin(pin)
+    elif GSM_MODULE is SIM868_WAVESHARE_GSM:
+        raise RuntimeError("Not Implemented")
+    elif GSM_MODULE is MOCK_STICK:
+        if pin == 5252:
+            return True
+        else:
+            return False
