120d02a0f219fb64afa8d17f5b21f717a3b4baa7
Jascha Riedel <jaschelite@googlemail.com>
2019-10-19 15:34:25 +0200

    Started Wifi Command

--- backend/commands.py
+++ backend/commands.py
@@ -1,7 +1,9 @@
 
-
+import subprocess
 
 def tryConnectToWiFi(ssid: str, password: str):
+    subprocess.check_output(['ifdown', 'wlan0'])
+    subprocess.check_output(['wpa_supplicant', '-d', '-c', ''])
     pass
 
 
