98a13ebbfee09203bff7a5f8f53417a327e31f9c
Jascha Riedel <jaschelite@googlemail.com>
2019-10-19 12:26:19 +0200

    Fixed GSM adapter

--- backend/gsmadapter.py
+++ backend/gsmadapter.py
@@ -1,5 +1,4 @@
-import huaweiaccess
-import sim868WaveshareAccess
+
 import os
 
 
@@ -8,14 +7,19 @@ MOCK_STICK = -1
 HUAWEI_LTE_STICK = 0
 SIM868_WAVESHARE_GSM = 1
 
-if os.get('GSM_STICK') is None:
+if os.environ.get('GSM_STICK') is None:
     GSM_MODULE = MOCK_STICK
 else:
     try:
-        GSM_MODULE = int(os.get('GSM_STICK'))
+        GSM_MODULE = int(os.environ.get('GSM_STICK'))
     except Exception:
         print("Enviornment Variable GSM_STICK must be -1,0,1!!!")
 
+
+if GSM_MODULE is HUAWEI_LTE_STICK:
+    import huaweiaccess
+elif GSM_MODULE is SIM868_WAVESHARE_GSM:
+    import sim868WaveshareAccess
 
 
 
