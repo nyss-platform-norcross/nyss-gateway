13679cb22f586cf2f25583af879f64d1b7f39ae1
Jascha <jaschelite@googlemail.com>
2020-01-24 17:18:55 +0000

    Implemnted Iface status and WiFi connection

--- src/raspberry/wifihandler.py
+++ src/raspberry/wifihandler.py
@@ -1,7 +1,33 @@
-from gsm import SmsListener, RawSMS
+# from gsm import SmsListener, RawSMS
+import os
+import logging
+import subprocess
 
-def _addNewWifiSettings(ssid: str, password: str):
-    print("Adding new Wifi. SSID={} - Password={}".format(ssid, password))
+_log = logging.getLogger("WIFI-Handler")
+
+
+def _write_to_wpa_suuplicant(ssid: str, password: str):
+    wifiConfig = '\nnetwork={{\n    ssid="{}"\n    psk="{}"\n}}\n'.format(
+        ssid, password)
+
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", 'a+') as wpa:
+        wpa.write(wifiConfig)
+    subprocess.run(["wpa_cli", "-i", "wlan0", "reconfigure"])
+
+
+def addNewWifiSettings(ssid: str, password: str):
+    _log.debug("Trying to add wifi configuration: {}:{}".format(ssid, password))
+
+    if ssid.find('"') != -1:
+        raise ValueError(
+            "SSID may not contain quotation marks for security reasons")
+    if password.find('"') != -1:
+        raise ValueError(
+            "WiFi passsword may not contain quotation marks for security reasons")
+
+    _write_to_wpa_suuplicant(ssid, password)
+
+    _log.debug("Wifi Settings added")
 
 
 @SmsListener
@@ -10,9 +36,6 @@ def handleWifiSMS(sms: RawSMS):
         parts = sms.text.split(':')
         ssid = parts[1]
         password = parts[2]
-        _addNewWifiSettings(ssid, password)
+        addNewWifiSettings(ssid, password)
     else:
         return
-
-
-
