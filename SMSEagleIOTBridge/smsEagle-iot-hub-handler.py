93ce0ba52e682462e5e45df5493b3ff4c38c9d5c
nikolai <nikolai.gabriel.riedel@redcross.no>
2020-02-27 14:12:12 +0100

    Add logging configuration.

--- SMSEagleIOTBridge/smsEagle-iot-hub-handler.py
+++ SMSEagleIOTBridge/smsEagle-iot-hub-handler.py
@@ -7,13 +7,18 @@ import logging
 
 #The connection string can be found in azure iot hub 
 conn_str = ""
+logging.basicConfig(
+    filename='/var/log/iot-bridge-log.log',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 def send_sms(params):
     return str(requests.get('https://localhost/index.php/http_api/send_sms', params=params, verify=False))
 
 if __name__ == "__main__":
     #logging.basicConfig(filename='/var/log/iot-bridge-log.log', level=logging.DEBUG)
-    nyssIotBridge.init(send_sms, "")
+    nyssIotBridge.init(send_sms, conn_str)
     while True:
         time.sleep(1)
 
