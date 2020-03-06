cddb563351cd47f1fa56385077c0ae25797a4fb3
Samson Valland <samson.valland@clave.no>
2020-03-06 11:28:41 +0100

    Add possiblity to pass in eagle credentials and iothub connection string as either parameters or using environment variables (arguments has highest precedence)

--- SMSEagleIOTBridge/smsEagle-iot-hub-handler.py
+++ SMSEagleIOTBridge/smsEagle-iot-hub-handler.py
@@ -4,6 +4,7 @@ import time
 import logging
 import os
 import subprocess
+import sys
 
 # Python 3 needs to be installed on the SMSEagle to start this script, using "do-not-use_apt-get"
 # do-not-use_apt-get update
@@ -11,13 +12,16 @@ import subprocess
 # do-not-use_apt-get install python3-pip
 # pip3 install azure-iot-device
 
-#The connection string can be found in azure iot hub 
-conn_str = ""
-login = ""
-cred = ""
+def get_sys_arg(i):
+    sys.argv[i] if i < len(sys.argv) else None
+
+# The connection string can be found in azure iot hub
+conn_str = get_sys_arg(1) or os.environ["IOT_HUB_CONNECTIONSTRING"]
+login = get_sys_arg(2) or os.environ["SMSEAGLE_USERNAME"]
+cred = get_sys_arg(3) or os.environ["SMSEAGLE_PWD"]
 
 logging.basicConfig(
-    filename='/var/log/iot-bridge-log.log',
+    filename='iot-bridge-log.log',
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.DEBUG,
     datefmt='%Y-%m-%d %H:%M:%S')
@@ -38,7 +42,6 @@ def get_local_ips(params):
     return str(subprocess.getoutput('hostname -I'))
 
 if __name__ == "__main__":
-    #logging.basicConfig(filename='/var/log/iot-bridge-log.log', level=logging.DEBUG)
     nyssIotBridge.init(send_sms, ping_device, reboot_device, get_local_ips, conn_str)
     while True:
         time.sleep(1)
