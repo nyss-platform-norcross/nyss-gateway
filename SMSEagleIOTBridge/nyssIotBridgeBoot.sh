291cfe74a16ad165ac167320e509f6e62b2a6004
gardnk <gard.njatun.kroyer@redcross.no>
2020-08-27 10:40:03 +0200

    Fetch iot hub handler script including version on startup of the bridge service
    - Add direct method to get version number of the script
    - Add shell script to run on startup of the bridge service

---
+++ SMSEagleIOTBridge/nyssIotBridgeBoot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Updating IoT Hub handler script"
+
+curl -s -o /home/pi/smsEagle-iot-hub-handler.py https://raw.githubusercontent.com/nyss-platform-norcross/nyss-sms-gateway/master/SMSEagleIOTBridge/smsEagle-iot-hub-handler.py
+res=$?
+if test "$res" != "0"
+then
+    echo "Failed to update IoT Hub handler script"
+else
+    chmod +x /home/pi/smsEagle-iot-hub-handler.py
+    echo "Successfully updated IoT Hub handler script"
+    # save script version as latest commit id
+    curl -s https://api.github.com/repos/nyss-platform-norcross/nyss-sms-gateway/commits | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['sha'])" > /home/pi/iot-hub-handler-version.txt
+fi
+
+until ping -c1 azure.microsoft.com;
+do sleep 5;
+done;
